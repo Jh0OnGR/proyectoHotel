fc03635e144b3ffe1eb46af786d2b72e1f5ede9c
Angelica Garcia <70681219+YessBlack@users.noreply.github.com>
2021-10-10 09:44:57 -0500

    .

--- js/scroll.js
+++ js/scroll.js
@@ -2,7 +2,11 @@ window.addEventListener("scroll", function(){
     let header = document.getElementById("header");
     let btnSignIn = document.getElementById("btn-sign-in");
     let totalWidth = this.document.getElementById("totalWidth");
+    let btnFloating = this.document.getElementById("reserva");
+
     header.classList.toggle("header-abajo",window.scrollY>0);
     btnSignIn.classList.toggle("btn-abajo",window.scrollY>0);
     totalWidth.classList.toggle("container"),window.scrollY>0;
+    btnFloating.classList.toggle("position-fixed"),this.window.scrollY>0;
+    btnFloating.classList.toggle("showBtn"),this.window.scrollY>0;
 })
